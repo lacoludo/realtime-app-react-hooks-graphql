8e849903f1ca6c941863bd2bddda02ace878cb75
lacoludo <email@lacoludo.com>
2019-09-18 00:28:41 +0200

    Add Google login button

--- client/src/components/Auth/Login.js
+++ client/src/components/Auth/Login.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { GoogleLogin } from 'react-google-login';
 import { withStyles } from '@material-ui/core/styles';
 // import Typography from "@material-ui/core/Typography";
 
 const Login = ({ classes }) => {
-  return <div>Login</div>;
+  const onSuccess = googleUser => {
+    const idToken = googleUser.getAuthResponse().id_token;
+    console.log({ idToken });
+  };
+  return (
+    <GoogleLogin
+      clientId="235220928806-m94s7act4dgncr1lhknees5d741kv53u.apps.googleusercontent.com"
+      onSuccess={onSuccess}
+      isSignedIn={true}
+    />
+  );
 };
 
 const styles = {
