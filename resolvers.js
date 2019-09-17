8dce323cf26095a79fa60c5c2b8d8adb1c3e2260
lacoludo <email@lacoludo.com>
2019-09-18 01:10:03 +0200

    Authenticate users from Apollo Server

--- resolvers.js
+++ resolvers.js
@@ -1,3 +1,5 @@
+const { AuthenticationError } = require('apollo-server');
+
 const user = {
   _id: '7',
   name: 'Ludovic',
@@ -5,8 +7,15 @@ const user = {
   picture: 'https://cloudinary.com/lacoludo'
 };
 
+const authenticated = next => (root, args, ctx, info) => {
+  if (!ctx.currentUser) {
+    throw new AuthenticationError('You must me logged in');
+  }
+  return next(root, args, ctx, info);
+};
+
 module.exports = {
   Query: {
-    me: () => user
+    me: authenticated((root, args, ctx) => ctx.currentUser)
   }
 };
