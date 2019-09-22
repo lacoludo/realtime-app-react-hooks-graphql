543bbcf8ba07c993ea68c3c56be136382a3e49af
lacoludo <email@lacoludo.com>
2019-09-22 07:06:33 +0200

    Create and style the map

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,11 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
+import ReactMapGL, { NavigationControl } from 'react-map-gl';
 import { withStyles } from '@material-ui/core/styles';
 // import Button from "@material-ui/core/Button";
 // import Typography from "@material-ui/core/Typography";
 // import DeleteIcon from "@material-ui/icons/DeleteTwoTone";
 
+const INITIAL_VIEWPORT = {
+  latitude: 48.735443,
+  longitude: 2.295592,
+  zoom: 15
+};
+
 const Map = ({ classes }) => {
-  return <div>Map</div>;
+  const [viewport, setViewport] = useState(INITIAL_VIEWPORT);
+
+  return (
+    <div className={classes.root}>
+      <ReactMapGL
+        width="100vw"
+        height="calc(100vh - 64px)"
+        mapStyle="mapbox://styles/mapbox/streets-v9"
+        mapboxApiAccessToken="pk.eyJ1IjoibGFjb2x1ZG8iLCJhIjoiY2swdWh0cXBpMDN6bTNtb2M4b3U0dXY2bSJ9.cZANPbc9e15rpTsG8p1d8g"
+        onViewportChange={newViewport => setViewport(newViewport)}
+        {...viewport}
+      >
+        {/* Navigation Control */}
+        <div className={classes.navigationControl}>
+          <NavigationControl
+            onViewportChange={newViewport => setViewport(newViewport)}
+          />
+        </div>
+      </ReactMapGL>
+    </div>
+  );
 };
 
 const styles = {
